b2e406b300bad00fba87412fc7a74329838a0e06
jD91mZM2 <me@krake.one>
2020-08-05 12:35:04 +0200

    Build float example without sse

--- binaries/float.c
+++ binaries/float.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 int main() {
-  float f1 = 0.1;
-  float f2 = 0.2;
+  volatile float f1 = 0.1;
+  volatile float f2 = 0.2;
   float f3 = f1 + f2;
   printf("%g + %g = %g\n", (double) f1, (double) f2, (double) f3);
 
-  double d1 = 0.1;
-  double d2 = 0.2;
+  volatile double d1 = 0.1;
+  volatile double d2 = 0.2;
   double d3 = d1 + d2;
   printf("%g + %g = %g\n", d1, d2, d3);
 }
